ce6762699d1a04a7f824b47750f059cac6959b22
Berat Ferit Özgül <beratozgull44@outlook.com>
2025-02-25 13:19:44 +0100

    understanding Prop types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import {Person} from './components/Person'
+import {PersonList} from './components/PersonList'
 function App() {
+  const personName={
+    first:'Bruce',
+    last:'Wayne',
+  }
+  const nameList=[
+    {
+      first:'Bruce',
+      last:'Wayne'
+    },
+    {
+      first:'Clark',
+      last:'Kent'
+    },
+    {
+      first:'Diana',
+      last:''
+    },
+  ]
   return (
     <div className="App">
-        <Greet name='Berat'/>
+        <Greet name='Berat' messageCount={20} isLoggedIn={false}/>
+        <Person name={personName}/>
+        <PersonList names={nameList}/>
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,10 +1,14 @@
 type GreetProps ={
     name: string
+    messageCount: number
+    isLoggedIn: boolean
 }
 export const Greet = (props:GreetProps) =>{
     return (
         <div>
-            <h2>Welcome {props.name}! Welcome to Here!</h2>
+            <h2>
+                {props.isLoggedIn ? 'Welcome {props.name}! Welcome to Here! You have {props.messageCount} mesages':'Welcome Guest'}
+            </h2>
         </div>
     )
 }
