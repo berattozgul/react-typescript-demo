97f38b16a0b21049d2a7c67ce178a9d845184e04
Berat Ferit Özgül <beratozgull44@outlook.com>
2025-02-25 16:01:01 +0100

    understanding useContext hooks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css';
-import { Greet } from './components/Greet';
-import {Person} from './components/Person'
-import {PersonList} from './components/PersonList'
-import {Status} from './components/Status'
-import {Heading} from './components/Heading'
-import {Oscar} from './components/Oscar'
-import {Button} from './components/Button'
-import { Input } from './components/Input';
-import { Container } from './components/Container';
+import { ThemeContextProvider } from './components/context/ThemeContext';
+import { Box } from './components/context/Box';
+import { UserContextProvider } from './components/context/UserContext';
+import { User } from './components/context/User';
 function App() {
   return (
     <div className="App">
-      
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
     </div>
   );
 }
