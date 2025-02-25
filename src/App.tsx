10eb202f19de5fe9d44e5c8fbfd478ae33336bb5
Berat Ferit Özgül <beratozgull44@outlook.com>
2025-02-25 17:38:28 +0100

    understanding extracting a components prop types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css';
-import { CustomButton } from './components/html/Button';
 
 function App() {
   return (
     <div className="App">
-      <CustomButton variant="primary" >selam</CustomButton>
+      
     </div>
   );
 }
