3f88c4092d8538c5609160e8b67fddc08d192bac
Berat Ferit Özgül <beratozgull44@outlook.com>
2025-02-25 16:31:46 +0100

    understanding Class Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { ThemeContextProvider } from './components/context/ThemeContext';
 import { Box } from './components/context/Box';
 import { UserContextProvider } from './components/context/UserContext';
 import { User } from './components/context/User';
+import { Counter } from './components/class/Counter';
 function App() {
   return (
     <div className="App">
-
+      <Counter message='Hello Darkness my old friend'/>
     </div>
   );
 }
